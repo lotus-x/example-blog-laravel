5ae48de82c10697391a775cd44d36c9e2ecf68b6
lotus-x <isuruimesh@gmail.com>
2023-08-10 20:59:59 +0530

    Added login and register views

---
+++ resources/views/login.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.base')
+
+@section('content')
+    <div class="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+        <div class="w-full max-w-sm space-y-10">
+            <div>
+                <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    alt="Your Company">
+                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your
+                    account</h2>
+            </div>
+            <form class="space-y-6" action="{{ route('login') }}" method="POST">
+                @csrf
+                <div class="relative -space-y-px rounded-md shadow-sm">
+                    <div class="pointer-events-none absolute inset-0 z-10 rounded-md ring-1 ring-inset ring-gray-300"></div>
+                    <div>
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required
+                            class="relative block w-full rounded-t-md border-0 py-1.5 px-3 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Email address">
+                        @if ($errors->has('email'))
+                            <span class="text-xs text-red-600 mx-3">{{ $errors->first('email') }}</span>
+                        @endif
+                    </div>
+                    <div>
+                        <label for="password" class="sr-only">Password</label>
+                        <input id="password" name="password" type="password" autocomplete="current-password" required
+                            class="relative block w-full rounded-b-md border-0 py-1.5 px-3 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Password">
+                        @if ($errors->has('password'))
+                            <span class="text-xs text-red-600">{{ $errors->first('password') }}</span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="flex items-center justify-between">
+                    <div class="flex items-center">
+                        <input id="remember-me" name="remember-me" type="checkbox"
+                            class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                        <label for="remember-me" class="ml-3 block text-sm leading-6 text-gray-900">Remember me</label>
+                    </div>
+
+                    <div class="text-sm leading-6">
+                        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit"
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+                        in</button>
+                </div>
+            </form>
+
+            <p class="text-center text-sm leading-6 text-gray-500">
+                Don't have an account?
+                <a href="{{ route('register-view') }}" class="font-semibold text-indigo-600 hover:text-indigo-500">Sign up
+                    here</a>
+            </p>
+        </div>
+    </div>
+@endsection()
