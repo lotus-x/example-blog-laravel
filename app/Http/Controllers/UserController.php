0cade4c0e1c17151e2ce1105402bbe3c8fc8bd01
lotus-x <isuruimesh@gmail.com>
2023-08-10 21:01:07 +0530

    Added User Controller
    Added necessary requests to use controller
    Added login and register functionality

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Requests\UserLoginRequest;
+use App\Http\Requests\UserRegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+
+    public function loginView()
+    {
+        return view("login");
+    }
+
+    public function registerView()
+    {
+        return view("register");
+    }
+
+    public function login(UserLoginRequest $request)
+    {
+        $request->validated();
+
+        $email=$request->string('email')->trim();
+        $password=$request->string('password')->trim();
+
+        if (Auth::attempt(['email'=>$email,'password'=>$password])) {
+            $request->session()->regenerate();
+
+            return redirect()->intended(route('articles'));
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
+    public function register(UserRegisterRequest $request)
+    {
+        $request->validated();
+
+        $user=new User();
+
+        $user->name=$request->input('name');
+        $user->email=$request->input('email');
+        $user->password=$request->input('password');
+
+        $user->save();
+
+        return redirect()->route("login-view");
+    }
+}
---
+++ app/Http/Requests/UserRegisterRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=>["required","string"],
+            'email'=>["required","email"],
+            'password'=>['required', 'string', 'same:re-password'],
+            're-password'=>['required', 'string'],
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'name' => 'User name',
+            'email' => 'Email address',
+            'password' => 'Password',
+            're-password' => 'Password confirmation',
+        ];
+    }
+}
